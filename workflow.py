bac6d920484ce4e65c3ccb35317f35f17816b2c6
JohanneGottenborg <143721739+JohanneGottenborg@users.noreply.github.com>
2025-05-14 16:44:07 +0200

    Delete workflow.py

--- workflow.py
+++
@@ -1,6 +0,0 @@
-#!/bin/env python3
-import sys, os
-sys.path.insert(0, os.path.realpath('/faststorage/project/ostrich_thermal/people/johanneg/workflows/mapping_and_filtering/workflow_source/'))
-from mapping_filtering_workflow_source import *
-
-gwf = filtering_raw_reads_mapping_filtering_bams()
